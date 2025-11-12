db19bc30b121b33add11c62fb320111e5220a5eb
Alberto M. <keleto2004@gmail.com>
2025-11-12 21:03:54 +0100

    Add list_files endpoint to list cloudisk dir

--- cloudisk/api/config.py
+++ cloudisk/api/config.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI, HTTPException, Request
 
 from ..config import config
+from . import get_logger
 
-TAGS = [{"files": "Operations related to files management."}]
+TAGS = [{"name": "files", "description": "Operations related to files management."}]
 
 
-files = APIRouter(prefix="/files", tags=["files"])
+logger = get_logger("cloudisk.api")
+
 
 app = FastAPI(
     title="cloudisk_API",
@@ -14,4 +16,9 @@ app = FastAPI(
     openapi_tags=TAGS,
 )
 
-app.include_router(files)
+
+# Manage exceptions, to return a JSON
+@app.exception_handler(Exception)
+async def general_exception_handler(request: Request, e: Exception):
+    logger.critical(f"Unhandled exception occurred: {e}")
+    return HTTPException(500, {"detail": "Internal server error"})
