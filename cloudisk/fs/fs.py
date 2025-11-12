8b827cd3df301fb53a2b0c9dd0b64dc5ca2b6a12
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-11-12 20:13:14 +0100

    Fixed minor inconsistencies

--- cloudisk/fs/fs.py
+++ cloudisk/fs/fs.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 from cloudisk import logger
-from .utils import check_path_type
+from .utils import remove_path
 
 CLOUDISK_DIR = ".cloudisk"
 CLOUDISK_ROOT = Path.home() / CLOUDISK_DIR
@@ -10,7 +10,7 @@ CLOUDISK_ROOT = Path.home() / CLOUDISK_DIR
 
 def init_file_structure(path: Path) -> bool:
     # Handle it asking for user consent
-    if path.exists() and not check_path_type(path) is False:
+    if path.exists() and not remove_path(path):
         return False
 
     path.mkdir()
