ac5dc96bf21be5339f7a45b7d6d1fc49d70c6101
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-11-26 01:04:50 +0100

    Migrated argument parser to make it more flexible

--- cloudisk/cli/args.py
+++ cloudisk/cli/args.py
@@ -1,60 +1,124 @@
-import argparse
+from argparse import ArgumentParser, Namespace, _SubParsersAction
 from enum import StrEnum, auto
 from pathlib import Path
+from typing import Annotated, Any, Callable, Literal
 
-from cloudisk.vars import CLOUDISK_ROOT
+from pydantic import AfterValidator, BaseModel, Field
 
-PATH_FLAGS = ("-p", "--path")
+from cloudisk.logger import get_logger
+
+logger = get_logger("cloudisk.args")
 
 
-class Command(StrEnum):
+class CommandName(StrEnum):
     INIT = auto()
     LINK = auto()
     UNLINK = auto()
     RUN = auto()
 
 
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        prog="cloudisk",
-        description="Uncentralized content distribution system, on your own cloud",
-    )
+class Flag(BaseModel):
+    short: str = Annotated[Field(frozen=True), AfterValidator("_validate_short")]
+    long: str = Annotated[Field(frozen=True), AfterValidator("_validate_long")]
+    type: object
+    help: str = ""
+    required: bool = True
 
-    subparsers = parser.add_subparsers(dest="command", required=True)
+    @classmethod
+    def _validate_short(cls, short: str) -> str:
+        if short.replace("-", "") != short:
+            raise ValueError("Please provide flags without dashes")
 
-    subparsers.add_parser(
-        Command.INIT.value,
-        help=f"Creates the basic configuration to run cloudisk at '{CLOUDISK_ROOT}'",
-    )
+        if len(short) > 1:
+            raise ValueError("Short flags must be 1 character long")
 
-    link_parser = subparsers.add_parser(
-        Command.LINK.value,
-        help=f"Creates a symlink inside {CLOUDISK_ROOT}",
-    )
-    link_parser.add_argument(
-        *PATH_FLAGS,
-        type=Path,
-        required=True,
-        help=(
-            "If path is a file, creates a symlink to that file."
-            "If path is a dir, creates a symlink for each file/dir inside itself."
-        ),
-    )
+        return short
 
-    unlink_parser = subparsers.add_parser(
-        Command.UNLINK.value,
-        help=f"Removes a symlink inside {CLOUDISK_ROOT}",
-    )
-    unlink_parser.add_argument(
-        *PATH_FLAGS,
-        type=Path,
-        required=True,
-        help="Path can refer to a file or dir",
-    )
+    @classmethod
+    def _validate_long(cls, long: str) -> str:
+        if long.replace("-", "") != long:
+            raise ValueError("Please provide flags without dashes")
 
-    subparsers.add_parser(
-        Command.RUN.value,
-        help="Runs the cloudisk server on 0.0.0.0:8000",
-    )
+        if len(long) < 3:
+            raise ValueError("Long flags must be 3 characters long or more")
 
-    return parser.parse_args()
+        return long
+
+
+class RequiredFlag(Flag):
+    required: Literal[True] = Field(default=True, frozen=True)
+
+    def __init__(self, **data):  # noqa: D107
+        base: Flag = data.pop("base", None)
+
+        if base:
+            data.update(base.model_dump())
+
+        super().__init__(**data)
+
+
+class OptionalFlag(Flag):
+    required: Literal[False] = Field(default=False, frozen=True)
+
+
+class Command(BaseModel):
+    name: CommandName
+    help: str
+    action: Callable
+    flags: list[Flag] = Field(default_factory=list)
+    __parser: ArgumentParser
+
+    def attach(self, parser: _SubParsersAction):
+        self.__parser: ArgumentParser = parser.add_parser(
+            self.name.value,
+            help=self.help,
+        )
+
+        if not self.flags:
+            return
+
+        for flag in self.flags:
+            model = flag.model_dump()
+            flags = (model.pop("short"), model.pop("long"))
+
+            self.__parser.add_argument(*flags, **model)
+
+    def run(self, *args, **kwargs):
+        self.action(*args, **kwargs)
+
+
+class Parser(BaseModel):
+    name: str
+    description: str
+    commands: list[Command]
+    __parser: ArgumentParser
+
+    def model_post_init(self, context: Any):  # noqa: D102
+        self.__parser = ArgumentParser(
+            prog=self.name,
+            description=self.description,
+        )
+
+        subparsers = self.__parser.add_subparsers(dest="command", required=True)
+
+        for command in self.commands:
+            command.attach(subparsers)
+
+    def parse(self) -> Namespace:
+        return self.__parser.parse_args()
+
+    def dispatch(self):
+        self.run(self.parse())
+
+    def run(self, arguments: Namespace):
+        _args = vars(arguments)
+        _command = _args.pop("command")
+
+        for command in self.commands:
+            if command.name == _command:
+                return command.action(**_args)
+
+        raise ValueError(f"There is no command associated to {_command}")
+
+
+PATH_FLAGS = Flag(short="-p", long="--path", type=Path)
---
+++ cloudisk/cli/parser.py
@@ -0,0 +1,48 @@
+from cloudisk.cli.args import PATH_FLAGS, Command, CommandName, Parser, RequiredFlag
+from cloudisk.fs.commands import init_cloudisk_folder, link_path, unlink_path
+from cloudisk.http import server
+from cloudisk.vars import CLOUDISK_ROOT
+
+
+def run():
+    Parser(
+        name="cloudisk",
+        description="A decentralized content distribution system. Run your own cloud.",
+        commands=[
+            Command(
+                name=CommandName.INIT,
+                help=f"Creates the basic configuration to run cloudisk at '{CLOUDISK_ROOT}'",  # noqa: E501
+                action=init_cloudisk_folder,
+            ),
+            Command(
+                name=CommandName.LINK,
+                help=f"Creates a symlink inside {CLOUDISK_ROOT}",
+                action=link_path,
+                flags=[
+                    RequiredFlag(
+                        base=PATH_FLAGS,
+                        help=(
+                            "If path is a file, creates a symlink to that file."
+                            "If path is a dir, creates a symlink for each file/dir inside itself."  # noqa: E501
+                        ),
+                    )
+                ],
+            ),
+            Command(
+                name=CommandName.UNLINK,
+                help=f"Removes a symlink inside {CLOUDISK_ROOT}",
+                action=unlink_path,
+                flags=[
+                    RequiredFlag(
+                        base=PATH_FLAGS,
+                        help="Path can refer to a file or dir",
+                    )
+                ],
+            ),
+            Command(
+                name=CommandName.RUN,
+                action=server.run,
+                help="Runs the cloudisk server on 0.0.0.0:8000",
+            ),
+        ],
+    ).dispatch()
