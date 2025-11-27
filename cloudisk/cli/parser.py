7862dd7fddc0b43356ac6ad0af0bad3bae949129
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-11-27 22:12:57 +0100

    Code cleanup: removed fs.decorators and reduced parser nesting

--- cloudisk/cli/parser.py
+++ cloudisk/cli/parser.py
@@ -7,48 +7,64 @@ from cloudisk.cli.args import (
 )
 from cloudisk.fs.commands import init_cloudisk_folder, link_path, unlink_path
 from cloudisk.http import server
+from cloudisk.logger import get_logger
 from cloudisk.vars import CLOUDISK_ROOT
+
+logger = get_logger("cloudisk.parser")
 
 
 def run():
-    Parser(
+    init = Command(
+        name=CommandName.INIT,
+        help=f"Creates the basic configuration to run cloudisk at '{CLOUDISK_ROOT}'",
+        action=init_cloudisk_folder,
+    )
+
+    link = Command(
+        name=CommandName.LINK,
+        help=f"Creates a symlink inside {CLOUDISK_ROOT}",
+        action=link_path,
+        flags=[
+            RequiredFlag(
+                base=PATH_FLAGS,
+                help=(
+                    "If path is a file, creates a symlink to that file."
+                    "If path is a dir, creates a symlink for each file/dir inside itself."
+                ),
+            ),
+        ],
+    )
+
+    unlink = Command(
+        name=CommandName.UNLINK,
+        help=f"Removes a symlink inside {CLOUDISK_ROOT}",
+        action=unlink_path,
+        flags=[
+            RequiredFlag(
+                base=PATH_FLAGS,
+                help="Path can refer to a file or dir",
+            )
+        ],
+    )
+
+    run = Command(
+        name=CommandName.RUN,
+        action=server.run,
+        help="Runs the cloudisk server on 0.0.0.0:8000",
+    )
+
+    parser = Parser(
         name="cloudisk",
         description="A decentralized content distribution system. Run your own cloud.",
         commands=[
-            Command(
-                name=CommandName.INIT,
-                help=f"Creates the basic configuration to run cloudisk at '{CLOUDISK_ROOT}'",  # noqa: E501
-                action=init_cloudisk_folder,
-            ),
-            Command(
-                name=CommandName.LINK,
-                help=f"Creates a symlink inside {CLOUDISK_ROOT}",
-                action=link_path,
-                flags=[
-                    RequiredFlag(
-                        base=PATH_FLAGS,
-                        help=(
-                            "If path is a file, creates a symlink to that file."
-                            "If path is a dir, creates a symlink for each file/dir inside itself."  # noqa: E501
-                        ),
-                    ),
-                ],
-            ),
-            Command(
-                name=CommandName.UNLINK,
-                help=f"Removes a symlink inside {CLOUDISK_ROOT}",
-                action=unlink_path,
-                flags=[
-                    RequiredFlag(
-                        base=PATH_FLAGS,
-                        help="Path can refer to a file or dir",
-                    )
-                ],
-            ),
-            Command(
-                name=CommandName.RUN,
-                action=server.run,
-                help="Runs the cloudisk server on 0.0.0.0:8000",
-            ),
+            init,
+            link,
+            unlink,
+            run,
         ],
-    ).dispatch()
+    )
+
+    try:
+        parser.dispatch()
+    except Exception as e:
+        logger.error(f"'{type(e).__name__}: {e}'")
