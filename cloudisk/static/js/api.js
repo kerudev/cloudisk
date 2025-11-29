b1ace2fcf7466481ab742689394b419f22de25eb
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-11-29 23:27:33 +0100

    Adapted frontend to download large files

---
+++ cloudisk/static/js/api.js
@@ -0,0 +1,52 @@
+import { downloadResponseBlob, processFiles } from "./utils.js";
+
+export const getFiles = async () => {
+    const response = await fetch(`/files${window.location.search}`);
+    const data = await response.json();
+
+    if (!response.ok) console.error(data.message);
+
+    processFiles(data["files"]);
+}
+
+/**
+ * Uploads files to the server.
+ *
+ * @param {DataTransferItemList} items - Files to upload.
+ * @returns {Response} Response received from the backend.
+ */
+export const upload = async items => {
+    const files = [...items].filter(item => item.kind === "file").map(file => file.getAsFile());
+
+    const body = new FormData();
+    files.forEach(file => body.append("files", file));
+
+    const response = await fetch("/files", { method: "POST", body });
+    const data = await response.json();
+
+    if (!response.ok) console.error(data.message);
+
+    processFiles(data["files"]);
+}
+
+export const download = async path => {
+    const current = new URLSearchParams(window.location.search);
+    const search = Object.fromEntries(current.entries());
+
+    const pathParam = (search?.path)
+        ? search.path + "/" + path
+        : path;
+
+    const params = new URLSearchParams({ path: pathParam });
+
+    const response = await fetch(`/files?${params.toString()}`);
+
+    if (response.headers.has("content-disposition")) {
+        await downloadResponseBlob(response);
+    } else {
+        const data = await response.json();
+        processFiles(data["files"]);
+
+        history.pushState({ path }, '', `/?${params.toString()}`);
+    }
+}
--- cloudisk/static/js/app.js
+++ cloudisk/static/js/app.js
@@ -1,92 +1,17 @@
-const newLink = text => {
-    const current = new URLSearchParams(window.location.search);
-    const search = Object.fromEntries(current.entries());
+import { getFiles, upload } from "./api.js";
 
-    const path = (search?.path)
-        ? search.path + "/" + text
-        : text;
-
-    const params = new URLSearchParams({ path });
-
-    const a = document.createElement("a");
-    a.textContent = text;
-    a.addEventListener('click', async e => {
-        e.preventDefault();
-
-        const res = await fetch(`/files?${params.toString()}`);
-        await downloadResponseBlob(res);
-    })
-
-    const li = document.createElement("li");
-    li.appendChild(a);
-
-    return li;
-};
-
-/**
- * Processes the value of the `Content-Disposition` header and returns an UTF-8
- * encoded string.
- *
- * More info: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Content-Disposition
- *
- * @param {string} header - Value of the `Content-Disposition` header.
- * @returns {string} UTF-8 encoded string.
- */
-const processContentDisposition = header => {
-    if (header.includes("filename="))
-        return header.split('filename=')[1].replaceAll('"', '');
-
-    if (header.includes("filename*=utf-8''"))
-        return decodeURIComponent(header.split("filename*=utf-8''")[1]);
-}
-
-const downloadResponseBlob = async response => {
-    const blob = await response.blob();
-    const blobUrl = window.URL.createObjectURL(blob);
-
-    const headers = Object.fromEntries(response.headers);
-    const name = processContentDisposition(headers['content-disposition']);
-
-    const downloader = Object.assign(document.createElement('a'), {
-        href: blobUrl,
-        download: name,
-    });
-
-    downloader.click();
-
-    window.URL.revokeObjectURL(blobUrl);
-};
+window.addEventListener("popstate", getFiles);
 
 window.addEventListener("dragenter", e => e.preventDefault());
 
 window.addEventListener("dragover", e => {
     e.preventDefault();
-    if (e.dataTransfer) e.dataTransfer.dropEffect = 'copy';
+    if (e.dataTransfer) e.dataTransfer.dropEffect = "copy";
 });
 
 window.addEventListener("drop", async e => {
     e.preventDefault();
-
-    const files = [...e.dataTransfer.items].filter(item => item.kind === "file").map(file => file.getAsFile());
-
-    const data = new FormData();
-    files.forEach(file => data.append("files", file));
-
-    const response = await fetch("/files", { method: "POST", body: data });
-    const result = await response.json();
-
-    if (!result.ok) console.error(result.message);
-
-    window.location.reload();
+    await upload(e.dataTransfer.items);
 });
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const res = await fetch(`/files${window.location.search}`);
-    const data = await res.json();
-
-    const ul = document.createElement("ul");
-    data['files'].forEach(file => ul.appendChild(newLink(file)));
-
-    const body = document.querySelector("body");
-    body.appendChild(ul);
-});
+document.addEventListener("DOMContentLoaded", getFiles);
