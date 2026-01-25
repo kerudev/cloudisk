c7c515fcb21bf2904aa91792acb443be0d3fc03a
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-01-26 00:45:55 +0100

    Add auth router and move conftest to tests root

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,16 @@ import pytest
 
 from cloudisk.vars import CLOUDISK_DB_FILE
 
+TEST_USER = "test_user"
+TEST_MAIL = "test@test.com"
+TEST_PASS = "p4ssw0rd"
+
 
 @pytest.fixture(autouse=True)
 def fake_db(tmp_path, monkeypatch):
     tmp_db = tmp_path / CLOUDISK_DB_FILE
 
     monkeypatch.setattr("cloudisk.db.models.base.CLOUDISK_DB_PATH", tmp_db)
+    monkeypatch.setattr("cloudisk.http.dependencies.CLOUDISK_DB_PATH", tmp_db)
 
     return tmp_db
