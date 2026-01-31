68d3be39818821fd8e12a21273c46b9096a245cc
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-01-31 22:51:23 +0100

    Move current static files inside templates/js

--- cloudisk/vars.py
+++ cloudisk/vars.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 CLOUDISK_ROOT = Path.home() / ".cloudisk"
 
-CLOUDISK_STATIC_DEFAULT = Path(__file__).parent.parent / "static"
+CLOUDISK_STATIC_DEFAULT = Path(__file__).parent.parent / "templates" / "js"
 CLOUDISK_STATIC_ENV = os.environ.get("CLOUDISK_STATIC")
 CLOUDISK_STATIC = Path(CLOUDISK_STATIC_ENV or CLOUDISK_STATIC_DEFAULT).resolve()
 
