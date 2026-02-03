d9b3eb05ae480a50366bc4ef7459fcea293074ad
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-02-03 23:00:46 +0100

    Add files table

--- templates/js/src/main.js
+++ templates/js/src/main.js
@@ -1,6 +1,9 @@
 import { loginForm } from "./routes/auth/ui.js";
+import { files } from "./routes/files/ui.js";
 import { parseCookies } from "./utils.js";
 
 if (!("user" in parseCookies())) {
     loginForm();
+} else {
+    files();
 }
