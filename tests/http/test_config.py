8fdca419f217b0ca5fbaace4927755f6b9ccb0a0
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-02-04 23:45:17 +0100

    Increase coverage for config.py

--- tests/http/test_config.py
+++ tests/http/test_config.py
@@ -2,8 +2,15 @@ import json
 from unittest.mock import Mock
 
 import pytest
+from fastapi import HTTPException
 
-from cloudisk.http.config import general_exception_handler
+from cloudisk.db.models.metadata import Metadata
+from cloudisk.db.models.user import User
+from cloudisk.http.config import (
+    general_exception_handler,
+    metadata_exception_handler,
+    user_exception_handler,
+)
 
 
 @pytest.mark.asyncio
@@ -18,3 +25,35 @@ async def test_general_exception_handler():
 
     content = json.loads(response.body)
     assert content["message"] == "Internal server error"
+
+
+@pytest.mark.asyncio
+async def test_user_exception_handler():
+    request = Mock()
+
+    with pytest.raises(HTTPException) as exc_info:
+        await user_exception_handler(
+            request,
+            User.Error("Internal server error"),
+        )
+
+    response = exc_info.value
+
+    assert response.status_code == 400
+    assert response.detail == "Internal server error"
+
+
+@pytest.mark.asyncio
+async def test_metadata_exception_handler():
+    request = Mock()
+
+    with pytest.raises(HTTPException) as exc_info:
+        await metadata_exception_handler(
+            request,
+            Metadata.Error("Internal server error"),
+        )
+
+    response = exc_info.value
+
+    assert response.status_code == 400
+    assert response.detail == "Internal server error"
