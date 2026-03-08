03a6a90a611d02c17db2eec53eedc11a136daf39
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-03-08 21:51:26 +0100

    Adapt Scope and Settings to use the space values

--- cloudisk/globals.py
+++ cloudisk/globals.py
@@ -5,17 +5,14 @@ from pathlib import Path
 from cloudisk.tools import Context, Scope
 from cloudisk.vars import CLOUDISK_DB_PATH
 
-root_scope = Scope(
-    "root",
-    engine_path=CLOUDISK_DB_PATH,
-    settings_module="cloudisk.tools._settings.default",
-)
+root = Scope("root", engine_path=CLOUDISK_DB_PATH)
 
-root_scope.settings.set_default(
+root.update_space()
+root.settings.set_default(
     STATIC_PATH=str(Path(__file__).parent.parent / "templates" / "js")
 )
 
-# TMP
-settings = root_scope.settings
+context = Context(scopes=[root])
 
-context = Context(scopes=[root_scope])
+# TMP
+settings = root.settings
